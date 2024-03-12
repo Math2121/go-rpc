56d2da88dc68c16f97fa2291385350789a85c284
Matheus da Silva de Paula <matheus.silva@startyourapp.com.br>
2024-03-12 20:24:57 -0300

    feat: create stream and bidirectional stream service

--- internal/service/category.go
+++ internal/service/category.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"io"
 
 	"github.com/Math2121/go-rpc/internal/database"
 	"github.com/Math2121/go-rpc/internal/pb"
@@ -53,10 +54,9 @@ func (c *CategoryService) ListCategories(ctx context.Context, input *pb.Blank) (
 	return &pb.CategoryList{Categories: categoriesResponse}, nil
 }
 
-
 func (c *CategoryService) GetCategory(ctx context.Context, input *pb.CategoryGetRequest) (*pb.Category, error) {
 	category, err := c.CategoryDb.FindAllCategoriesWithCourses(input.Id)
-		if err != nil {
+	if err != nil {
 		return nil, err
 	}
 
@@ -67,4 +67,62 @@ func (c *CategoryService) GetCategory(ctx context.Context, input *pb.CategoryGet
 	}
 
 	return categoryResponse, nil
+}
+
+func (c *CategoryService) CreateCategoryStream(stream pb.CategoryService_CreateCategoryStreamServer) error {
+
+	categories := &pb.CategoryList{}
+	for {
+		category, err := stream.Recv()
+
+		if err == io.EOF {
+			return stream.SendAndClose(categories)
+		}
+
+		if err != nil {
+			return err
+		}
+
+		categoryResult, err := c.CategoryDb.Create(category.Name, category.Description)
+
+		if err != nil {
+			return err
+		}
+
+		categories.Categories = append(categories.Categories, &pb.Category{
+			Id:          categoryResult.ID,
+			Name:        categoryResult.Name,
+			Description: categoryResult.Description,
+		})
+
+	}
+}
+
+func (c *CategoryService) CreateCategoryStreamBidirectional(stream pb.CategoryService_CreateCategoryStreamBidirectionalServer) error {
+	for {
+		category, err := stream.Recv()
+
+		if err == io.EOF {
+			return nil
+		}
+
+		if err == io.EOF {
+			return nil
+		}
+
+		categoryResult, err := c.CategoryDb.Create(category.Name, category.Description)
+		if err != nil {
+			return err
+		}
+
+		err = stream.Send(&pb.Category{
+			Id:          categoryResult.ID,
+			Name:        categoryResult.Name,
+			Description: categoryResult.Description,
+		})
+		if err != nil {
+			return err
+		}
+
+	}
 }
